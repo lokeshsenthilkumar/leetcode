67202be1cfa47aeac7763d1ebf71be5868bfa6f2
Lokesh Senthilkumar <66515225+lokeshsenthilkumar@users.noreply.github.com>
2021-09-07 11:04:26 +0530

    Time: 816 ms (28.95%), Space: 16.5 MB (89.66%) - LeetHub

---
+++ 01-matrix/01-matrix.py
@@ -0,0 +1,34 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        
+        r = len(mat) ; c = len(mat[0])
+        q = []
+        
+        for i in range(r):
+            for j in range(c):
+                if mat[i][j] == 0:
+                    q.append((i,j))
+                else:
+                    mat[i][j] = -1
+                    
+        dirs = [(-1,0),(1,0),(0,-1),(0,1)]
+                    
+        while q!=[]:
+            
+            n = len(q)
+            
+            for i in range(n):
+                curr = q.pop(0)
+                
+                for d in dirs:
+                
+                    x = curr[0] + d[0]
+                    y = curr[1] + d[1]
+                    
+                    if x<0 or y<0 or x>r-1 or y>c-1 or mat[x][y]!=-1:
+                        continue
+                    
+                    mat[x][y] = mat[curr[0]][curr[1]] + 1
+                    q.append((x,y))
+            
+        return mat
