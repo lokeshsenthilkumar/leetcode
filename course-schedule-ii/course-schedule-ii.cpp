b48dfa5db9670e0bbda09b824c50d9bc3dcbaae7
Lokesh Senthilkumar <66515225+lokeshsenthilkumar@users.noreply.github.com>
2021-09-11 23:10:38 +0530

    Time: 20 ms (74.69%), Space: 14.3 MB (37.77%) - LeetHub

---
+++ course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+           
+        vector<int> adj[n];
+        vector<int> output;
+        
+        for(auto edge : pre)
+            adj[edge[1]].push_back(edge[0]);
+        
+        vector<int> vis(n,0);
+        
+        for(int i=0;i<n;i++){
+            if(iscycle(adj,vis,i,output))
+                return {};
+        }
+        
+        reverse(output.begin(),output.end());
+        
+        return output;
+    }
+    
+    bool iscycle(vector<int> adj[],vector<int> &vis,int id,vector<int> &output){
+        
+        if(vis[id]==1)  // processing (present in current recursion stack)
+            return true;
+        
+        if(vis[id] == 2) // this check is not mandatory
+            return false;
+
+        if(vis[id] == 0){
+            vis[id] = 1;
+            for(auto edge : adj[id]){
+                if(vis[edge]!=2 && iscycle(adj,vis,edge,output))
+                    return true;
+            }
+        }
+
+        output.push_back(id);
+        
+        vis[id] = 2;  // processed (popping out of current recursion stack)
+        return false;
+    }
+};
