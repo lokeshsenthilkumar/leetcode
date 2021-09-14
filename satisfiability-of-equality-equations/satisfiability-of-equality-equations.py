39de7ce74dabab9eec4b76e06efe8b4f0630e991
Lokesh Senthilkumar <66515225+lokeshsenthilkumar@users.noreply.github.com>
2021-09-14 15:48:55 +0530

    Time: 57 ms (24.79%), Space: 14.5 MB (32.26%) - LeetHub

--- satisfiability-of-equality-equations/satisfiability-of-equality-equations.py
+++ satisfiability-of-equality-equations/satisfiability-of-equality-equations.py
@@ -2,29 +2,23 @@ class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
         
         def find(x):
-            return x if parent[x] == x else find(parent[x])
+            return x if x not in parent else find(parent[x])
         
         def union(i,j):
-            if find(i) != find(j):
-                parent[find(j)] = find(i)
+            fi = find(i) ; fj = find(j)
+            if fi!=fj:
+                parent[fj] = fi
         
-        parent = [i for i in range(256)]
-        
-        f = 0 
+        parent = {}
         
         for i in equations:
-            
             if i[1] == '=':
-                union(ord(i[0]),ord(i[-1]))
-                f = 1
+                union(i[0],i[-1])
                 
         
         for i in equations:
-            
             if i[1] == '!':
-                if i[0]==i[-1] or find(ord(i[0])) == find(ord(i[-1])):
+                if find(i[0]) == find(i[-1]):
                     return False
-                
         
-                
         return True
