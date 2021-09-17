86f567598fd0c3182a6688ac396ce91fedb8e1cf
Lokesh Senthilkumar <66515225+lokeshsenthilkumar@users.noreply.github.com>
2021-09-17 19:38:42 +0530

    Time: 28 ms (88.37%), Space: 14.2 MB (77.10%) - LeetHub

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -1,14 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        p2 = p1 = 0
+        p2 = 0 ; p1 = 0
         
         for i in range(len(nums)):
-            
-            t = p1
-            p1 = max(p1, nums[i]+p2)
-            p2 = t
-	        
+            p2 , p1 = p1 , max(p1, p2+nums[i])
+        
         return p1
         
-       
